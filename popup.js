c24f78ccfd069095f0daf7a864fb6ad17f1dccc5
Brendan Luna <brendanluna1@gmail.com>
2015-12-08 19:57:05 -0800

    Removed popup.js and view_jobs.html, both files out of use

--- popup.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2014 Brendan Luna. All rights reserved.
-// popup.js
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
